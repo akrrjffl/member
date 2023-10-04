791960ab244423327179628769cfbe3d771b7f66
akrrjffl <souij02060@naver.com>
2023-10-04 09:36:16 +0900

    완료

---
+++ src/main/java/com/icia/member/entity/MemberEntity.java
@@ -0,0 +1,69 @@
+package com.icia.member.entity;
+
+import com.icia.member.dto.MemberDTO;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter(AccessLevel.PRIVATE)    // 기본생성자를 막아주는 역할
+@Table(name = "member_table")
+public class MemberEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String memberEmail;
+
+    @Column(nullable = false)
+    private String memberPassword;
+
+    @Column(nullable = false)
+    private String memberName;
+
+    @Column
+    private String memberBirth;
+
+    @Column
+    private String memberMobile;
+
+    public static MemberEntity toSaveEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        memberEntity.setMemberBirth(memberDTO.getMemberBirth());
+        memberEntity.setMemberMobile(memberDTO.getMemberMobile());
+        return memberEntity;
+    }
+
+    public static MemberEntity toUpdateEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setId(memberDTO.getId());
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        memberEntity.setMemberBirth(memberDTO.getMemberBirth());
+        memberEntity.setMemberMobile(memberDTO.getMemberMobile());
+        return memberEntity;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
